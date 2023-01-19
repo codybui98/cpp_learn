446027cabf70bb23331d685a2fcba84e16ade4a4
codybui98 <buileanh98@gmail.com>
2023-01-19 17:47:11 +0700

    Hello World!

---
+++ helloworld.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // khai bao thu vien input output
+
+using namespace std; // khai bao ten
+
+//-----------------------------//
+//-----Kieu du lieu so---------//
+// int: 4 bytes = 32 bits (it chinh xac) //
+// long long : 8byte = 64bit (chinh xac cao) //
+// float: 4 bytes = 32bits (it chinh xac)//
+// double: 8bytes = 64bits (chinh xac cao) //
+// char : 1byte 
+// bool : 1byte//
+// pow(a,b) = a^b (kieu double) //
+
+
+int main() { // tra ve du lieu kieu int
+    cout << "Hello World! \n"; // syntax in ra man hinh, << dau output 
+    return 0;
+}
